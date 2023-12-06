090d8fb2db1170b306a5e00fc264ea56ff5e360c
Keishu Aruga <aruga@keishunoMacBook-Pro.local>
2023-12-06 19:03:23 +0900

    fix next page

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, request 
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.get("/")
 def index():
     return render_template('index.html')
 
-@app.route('/address', methods=['POST'])
+@app.route('/address', methods=['GET', 'POST'])
 def sample_form_temp():
     req1 = request.form['address']
     print(req1)
-    return render_template('map.html', address=req1)
+    if request.method == 'POST':
+        return render_template('map.html', address=req1)
+    else:
+        return render_template('index.html')
     
 
 
